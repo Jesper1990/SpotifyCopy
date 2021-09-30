096d3a7a07f6d576283a5bf38848b37de486d0ac
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2021-09-30 18:53:15 +0200

    Added comments about the functionality. Not a fully functional playlist.

--- web_app/src/components/Player/Player.jsx
+++ web_app/src/components/Player/Player.jsx
@@ -9,7 +9,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // TODO: Updatera spellistan så den kan starta från index inte från början!
 
 const Player = () => {
+  // State på 1 specifikt ID som skickas via onClick i sökfunktionen.
   const videoId = useSelector(state => state.videoId.videoId)
+
+  // State på en Array utav ID:s som skickas via onClick i sökfunktionen, skall separeras för bättre funktionalitet.
   const videoPlaylist = useSelector(state => state.videoPlaylist.videoPlaylist)
   const [player, setPlayer] = useState()
   const [isActive, setIsActive] = useState(false)
@@ -18,12 +21,14 @@ const Player = () => {
     loadPlayer()
   }, [])
 
+  // Startar spelaren med det specifika ID:t för låten man har klickat på.
   useEffect(() => {
     if (videoId) {
       startSong(videoId)
     }
   }, [videoId])
 
+  // Ej fullt funktionell då den nu alltid startar på första ID:t även om man klickar på någon annan.
   useEffect(() => {
     if (videoPlaylist) {
       startPlaylist(videoPlaylist)
@@ -40,7 +45,7 @@ const Player = () => {
     })
     setPlayer(ytPlayer)
   }
-
+  
   const onPlayerStateChange = (event) => {
     if (event.data != YT.PlayerState.PLAYING) return
   }
@@ -48,10 +53,11 @@ const Player = () => {
   const startSong = () => {
     player.loadVideoById(videoId)
   }
+  // Funktion för att ladda spelaren med en spellista (en array utav ID:s som skickas via store.)
   const startPlaylist = () => {    
     player.loadPlaylist(videoPlaylist)
   }
-
+  // Funktion för att ändra toggle på "Play / Pause" ikonerna
   const playSong = () => {
     if (!isActive) {
       setIsActive(true)
@@ -61,9 +67,11 @@ const Player = () => {
       player.pauseVideo()
     }
   }
+  // Funktion för knappen "Next"
   const nextSong = () => {
     player.nextVideo()
   }
+  // Funktion för knappen "Previous"
   const previousSong = () => {
     player.previousVideo()
   }
@@ -73,18 +81,14 @@ const Player = () => {
       <div id="yt-player"></div>
       <div className="buttons">
         <ul className="list-buttons">
-          <li className="list-link" onClick={previousSong}>
-           
-              <FontAwesomeIcon className="fa-icon" icon={faBackward} />
-            
+          <li className="list-link" onClick={previousSong}>          
+              <FontAwesomeIcon className="fa-icon" icon={faBackward} />           
           </li>
           <li className="list-play-pause" onClick={playSong}>
-            
-              {isActive ? <FontAwesomeIcon className="play-pause" icon={faPause}/> : <FontAwesomeIcon className="play-pause" icon={faPlay} /> }
-            
+          {/* Toggle funktion för att ändra display på ikoner. ? = True, : = False. "Play" är default eftersom useState är satt till false default.  */}  
+              {isActive ? <FontAwesomeIcon className="play-pause" icon={faPause}/> : <FontAwesomeIcon className="play-pause" icon={faPlay} /> }          
           </li>
-          <li className="list-link" onClick={nextSong}>         
-             
+          <li className="list-link" onClick={nextSong}>                     
               <FontAwesomeIcon className="fa-icon" icon={faForward} />
             
           </li>
