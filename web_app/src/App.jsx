0c368e25435726613546b3003d290ba773ffe073
Adnan Kovacevic <69817904+Ado-wq@users.noreply.github.com>
2021-09-30 08:44:07 +0200

    navbar fixed
    Navbar works on mobile and web, seachpage added to menu working..
    
    added googlefonts and fontawesome to HTML

--- web_app/src/App.jsx
+++ web_app/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 /*import GetUsers from './components/services/getUsers' */
 import Register from './components/Register/Register'
 import Start from './components/Start/Start';
-
+import Navbar from './components/Navbar/Navbar.jsx'
+import StartPage from './components/Pages/StartPage'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import SongSearch from './components/SongSearch/SongSearch';
 
@@ -14,17 +15,24 @@ function App() {
   return (
     <Router>
       <div className="App">
-      <nav>
-        <Link to="/register">Register</Link>        
-        <Link to="/search">Search</Link>
-      </nav>
 
-      <Route path="/register" component={Register} />
-      <Route path="/" exact component={Start} />
-      <Route path="/search" component={SongSearch}  />
+        <Navbar />
+
+        <nav>
+          <Link to="/register">Register</Link>
+          <Link to="/search">Search</Link>
+        </nav>
+
       </div>
-      </Router>
-     
+      <main>
+
+        <Route path="/register" component={Register} />
+        <Route path="/" exact component={Start} />
+        <Route path="/search" component={SongSearch} />
+        <Route path="/StartPage" exact component={StartPage} />
+      </main>
+    </Router>
+
   )
 }
 
