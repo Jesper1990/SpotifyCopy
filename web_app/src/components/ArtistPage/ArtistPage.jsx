f867f71ccb65f57dedbe37a203cf591af666d3e3
malmz90 <70096644+malmz90@users.noreply.github.com>
2021-10-06 15:08:36 +0200

    mapped out content from artistobjekt

--- web_app/src/components/ArtistPage/ArtistPage.jsx
+++ web_app/src/components/ArtistPage/ArtistPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
+import './ArtistPage.css'
 
 
  
@@ -7,6 +8,13 @@ function Artist() {
 
   const [artists, setArtists] = useState()
   let { browseid } = useParams()
+
+  const [popup, setPopup] = useState(false);
+
+  const togglePopup = () => {
+    setPopup(!popup)
+  }
+
 
   useEffect(() => {
     let canceled = false
@@ -26,13 +34,42 @@ function Artist() {
     }
   }, [])
   console.log("hej", artists)
-
+  
   
   return (
     <div>
            <h1>{artists && artists.name}</h1>
-      <p>{artists && artists.description}</p> 
+      <p>{artists && artists.description}</p>
+      <div>{artists && artists.products.albums.content.map(album => (
+        <div> {album.thumbnails[0].url} </div>
+        
+      ))}</div>
+      <p>{artists && artists.products.songs.content.map(song=> (
+        <div>{song.name}</div>
+      ))}</p>
+      
+       
+      <button onClick={togglePopup} className="btn-popup">Share Artist with a Link!</button>
+     
+      {popup && (
+        <div className="popup">
+        <div className="overlay"></div>
+        <div className="popup-content">
+            <h2>http://localhost:3000/artist/{browseid}</h2>
+
+            <button onClick={() => navigator.clipboard.writeText(`http://localhost:3000/artist/${browseid}`)} >
+            Copy Link!
+          </button>
+          
+          <button onClick={togglePopup} className="close-popup" >Close</button>
+        </div>
+        
     </div>
+      )}
+      
+    </div>
+   
+
   )
       }
 export default Artist;
