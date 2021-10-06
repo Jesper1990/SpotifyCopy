2534753d4c925bfba9f935bb986c617b51caf311
cmack77-dev <markus_863@hotmail.com>
2021-10-06 11:43:24 +0200

    minor changes login/register

--- web_app/src/components/Register/Register.jsx
+++ web_app/src/components/Register/Register.jsx
@@ -1,49 +1,58 @@
-import axios from "axios";
-import React, { useState } from 'react';
+import axios from 'axios'
+import React, { useState } from 'react'
 import './Register.css'
 
-function Register() {
-  const url = "http://localhost:8085/api/users"
+function Register () {
+  const url = 'http://localhost:8085/api/users'
   const [data, setData] = useState({
-    name: "",
-    email: "",
-    password:""
+    name: '',
+    //email: "",
+    password: ''
   })
 
-  function handle(e) {
+  function handle (e) {
     const newdata = { ...data }
     newdata[e.target.id] = e.target.value
     setData(newdata)
     console.log(newdata)
   }
 
-  function submit(e) {
-    e.preventDefault();
-    axios.post(url, {
-      name: data.name,
-      email: data.email,
-      password: data.password
-    })
-      .then(res =>{
-      console.log(res.data)
-    })
-    
+  function submit (e) {
+    e.preventDefault()
+    axios
+      .post(url, {
+        name: data.username,
+        //email: data.email,
+        password: data.password
+      })
+      .then(res => {
+        console.log(res.data)
+      })
   }
 
   return (
-    
-    <div className = "container">
-      <form className="form-group" onSubmit={(e) => submit(e)}>
-        <input onChange={(e) => handle(e)} id="email" value={data.email} placeholder="@Email" type="text"></input>
-        <input onChange={(e)=>handle(e)} id="name" value={data.name} placeholder="Username" type="text"></input>
-        <input onChange={(e)=>handle(e)} id="password" value={data.password} placeholder="Password" type="text"></input>
-        
-        <button className="btn">Submit</button>
+    <div className='container'>
+      <form className='form-group' onSubmit={e => submit(e)}>
+        <input
+          onChange={e => handle(e)}
+          id='name'
+          value={data.username}
+          placeholder='Username'
+          type='text'
+        ></input>
+        <input
+          onChange={e => handle(e)}
+          id='password'
+          value={data.password}
+          placeholder='Password'
+          type='password'
+        ></input>
+
+        <button className='btn'>Login</button>
         <p>Already a member? Login now</p>
       </form>
-      </div>
-      
-  );
+    </div>
+  )
 }
 
-export default Register;
+export default Register
